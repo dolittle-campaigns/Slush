c8ce12ccdee4e477172b9c17c1cba35cd6440872
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-02 20:05:34 +0100

    Deploy now dockerizes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sh ./dockerize.sh
 docker push dolittle/campaigns:slush
 kubectl patch deployment slush --namespace dolittle-campaigns -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
